ea4ccbcbb6a92d735ff27f290c36b320321e6c92
GabrielVallee09 <gabrielvallee09@gmail.com>
2020-09-10 22:10:36 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    distance_entre_min_et_maj =ord('a') - ord()
+    distance_entre_min_et_maj =ord('a') - ord('')
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
