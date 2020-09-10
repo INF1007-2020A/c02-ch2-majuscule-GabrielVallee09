ba7e7abd45dc30ea38018034ab0b47febdb3866f
GabrielVallee09 <gabrielvallee09@gmail.com>
2020-09-10 22:09:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    
+    distance entre min et maj = 32
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
